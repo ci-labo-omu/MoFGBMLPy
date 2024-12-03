2ef65e5519d8093d65c2d4b718b48e3899a7293f
Ayato <seb01090@st.osakafu-u.ac.jp>
2024-12-03 15:13:23 +0900

    Revert "objectはnum rulesに"
    This reverts commit be4fb86169a58e2f6937fe8358f70c8d0a7ff015.

--- src/mofgbmlpy/main/abstract_mofgbml_main.py
+++ src/mofgbmlpy/main/abstract_mofgbml_main.py
@@ -468,8 +468,7 @@ class AbstractMoFGBMLMain(ABC):
             plt.plot(err_test[:, 0], err_test[:, 1], c='blue', marker='o', label="Test")
         plt.xlabel(x_label)
         if title is not None:
-            #plt.title(title)
-            pass
+            plt.title(title)
         if grid:
             plt.grid()
 
