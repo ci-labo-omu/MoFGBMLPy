a6bbcd239e797bcb555a5da3bbf60689c674ab69
Ayato <seb01090@st.osakafu-u.ac.jp>
2025-01-10 16:56:31 +0900

    ルール重みはrule_basicの中で掛けられていたのでsingle_winner_rule_selectionを修正

--- src/mofgbmlpy/fuzzy/classification/single_winner_rule_selection.pyx
+++ src/mofgbmlpy/fuzzy/classification/single_winner_rule_selection.pyx
@@ -87,9 +87,6 @@ cdef class SingleWinnerRuleSelection(AbstractClassification):
             # if self.__cache_size == 0:  # No cache
 
             value = solution.get_fitness_value(pattern.get_attributes_vector())
-            # Value should be multiplied by the Rule weight.
-            rule_weight = solution.get_rule_weight().get_value()
-            value = value * rule_weight
 
             # else:
             #     value = self.get_fitness_value(solution, pattern)
