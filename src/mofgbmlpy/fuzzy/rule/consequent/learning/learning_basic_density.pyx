8bf046a3d1ffe0229b53393ea35fa77d472a8c29
Ayato <seb01090@st.osakafu-u.ac.jp>
2025-02-08 18:32:32 +0900

    learningにおいて，weightの逆数をかけるように変更

--- src/mofgbmlpy/fuzzy/rule/consequent/learning/learning_basic_density.pyx
+++ src/mofgbmlpy/fuzzy/rule/consequent/learning/learning_basic_density.pyx
@@ -85,7 +85,7 @@ cdef class LearningBasicDensity(AbstractLearningWithDensity):
             for i in range(dataset.get_size()):
                 pattern = patterns[i]
                 if pattern.get_target_class().get_class_label_value() == c:
-                    part_sum += compatible_grades[i]*densities[i]
+                    part_sum += compatible_grades[i] / densities[i]
                     #part_sum += compatible_grades[i]
             sum_compatible_grade_for_each_class[c] = part_sum
             all_sum += part_sum
