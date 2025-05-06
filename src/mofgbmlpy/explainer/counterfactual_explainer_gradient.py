Fixed derivative of change loss in GD approach

src/mofgbmlpy/explainer/counterfactual_explainer_gradient.py
@@ -55,14 +55,14 @@ class CounterFactualExplainerGradient:
     def compute_membership_area_data(self, mf_1_y, mf_2_y, step):
         union_value = np.zeros(mf_1_y.shape[0])
         intersection_value = np.zeros(mf_1_y.shape[0])
-        mf_1_highest_area = np.zeros(mf_1_y.shape[0])
-        mf_2_highest_area = np.zeros(mf_1_y.shape[0])
+        mf_2_smallest_length = np.zeros(mf_1_y.shape[0])
+        mf_2_highest_length = np.zeros(mf_1_y.shape[0])
 
         for fs_i in range(mf_1_y.shape[0]):
             intersection_value[fs_i] = 0
             union_value[fs_i] = 0
-            mf_1_highest_area[fs_i] = 0
-            mf_2_highest_area[fs_i] = 0
+            mf_2_smallest_length[fs_i] = 0
+            mf_2_highest_length[fs_i] = 0
 
             for i in range(mf_1_y.shape[1]):
                 y_val = max(mf_1_y[fs_i][i], mf_2_y[fs_i][i])
@@ -72,11 +72,11 @@ class CounterFactualExplainerGradient:
                 intersection_value[fs_i] += step * y_val
 
                 if mf_1_y[fs_i][i] > mf_2_y[fs_i][i]:
-                    mf_1_highest_area[fs_i] += step
+                    mf_2_smallest_length[fs_i] += step
                 else:
-                    mf_2_highest_area[fs_i] += step
+                    mf_2_highest_length[fs_i] += step
 
-        return intersection_value, union_value, mf_1_highest_area, mf_2_highest_area
+        return intersection_value, union_value, mf_2_smallest_length, mf_2_highest_length
 
     def loss_functions(self, intersection_values, union_values):
         # Confidence loss
@@ -145,8 +145,8 @@ class CounterFactualExplainerGradient:
         mf_params,
         intersection_value,
         union_value,
-        mf_1_highest_area,
-        mf_2_highest_area,
+        mf_current_smallest_length,
+        mf_current_highest_length,
     ):
         gradient = np.zeros((fs_mf_values.shape[1], 3), dtype=object)  # shape (num_fs, num_params)
 
@@ -155,49 +155,23 @@ class CounterFactualExplainerGradient:
             self._train_set, self._initial_class, self._target_class
         )
 
-        num_p_initial_class = len(patterns_idx_initial_class)
-        num_p_target_class = len(patterns_idx_target_class)
-
         patterns = self._train_set.get_patterns()
         num_p = len(patterns)
 
         sum_all_mf_values = np.sum(antecedent_mf_value)
 
-        sum_initial_class_mf_values = np.sum(antecedent_mf_value[patterns_idx_initial_class])
         sum_target_class_mf_values = np.sum(antecedent_mf_value[patterns_idx_target_class])
-
-
-        confidence_loss_derivative = 1
-        #
-        # # Avoid division by zero
-        # if sum_all_mf_values != 0:
-        #     confidence_loss_derivative = (
-        #         # num_p_initial_class * sum_all_mf_values
-        #         # - num_p * sum_initial_class_mf_values
-        #         - num_p_target_class * sum_all_mf_values
-        #         + num_p * sum_target_class_mf_values
-        #     ) / (sum_all_mf_values**2)
-        #
-        # gradient[:, :] = (
-        #     self._confidence_loss_weight * confidence_loss_derivative
-        # )
 
         # # dL_change/d_membership_aq
         loss_change_derivative = 0
         if intersection_value is not None and union_value is not None:
             for i in range(intersection_value.shape[0]):
-                loss_change_derivative += mf_1_highest_area[i] * union_value[i] - intersection_value[i] * mf_2_highest_area[i]
+                loss_change_derivative += (
+                    intersection_value[i] * mf_current_highest_length[i]
+                    - mf_current_smallest_length[i] * union_value[i]
+                ) / (union_value[i] ** 2)
 
         loss_change_derivative /= intersection_value.shape[0]
-
-        #######################################
-
-        # (d_membership_aq/d_membership_aqi) * (d_membership_aqi/d_mf_params)
-
-        # for p in patterns:
-        #     print(p.get_target_class().get_class_label_value())
-        #
-        # raise Exception("Stop here")
 
         for i in range(len(mf_params)):  # for all dimensions (i.e. fuzzy sets) in the antecedent
             if mf_params[i] is None or len(mf_params[i]) == 0:
@@ -221,16 +195,19 @@ class CounterFactualExplainerGradient:
 
                 is_target_class = 1 if p.get_target_class() == self._target_class else 0
 
-                confidence_loss_derivative = -(is_target_class * sum_all_mf_values - sum_target_class_mf_values) / (sum_all_mf_values**2)
+                confidence_loss_derivative = -(is_target_class * sum_all_mf_values - sum_target_class_mf_values) / (
+                    sum_all_mf_values**2
+                )
                 # confidence_loss_derivative = is_target_class
 
                 # d membership aqi / d mf params
                 for k in range(len(mf_params[i])):
                     # TODO: put it into the mf function class directly (here temporarily for testing)
                     derivative2 = self.get_param_derivative(k, mf_params[i], x[i])
-                    combined_loss_derivative = self._confidence_loss_weight * confidence_loss_derivative + (1 - self._confidence_loss_weight) * loss_change_derivative
-
-                    # print(f"Combined Loss Derivative: {combined_loss_derivative}, Confidence Loss Derivative: {confidence_loss_derivative}, Loss Change Derivative: {loss_change_derivative}")
+                    combined_loss_derivative = (
+                        self._confidence_loss_weight * confidence_loss_derivative
+                        + (1 - self._confidence_loss_weight) * loss_change_derivative
+                    )
 
                     mean_value[k] += combined_loss_derivative * derivative1 * derivative2
 
@@ -259,8 +236,6 @@ class CounterFactualExplainerGradient:
 
         self._fuzzy_rule.plot_antecedent()
 
-        prev_mf_values = None
-        intersection_value, union_value, mf_1_highest_area, mf_2_highest_area = None, None, None, None
         for epoch in range(self._max_num_epochs):
             # forward
             fs_mf_values = np.array(
@@ -277,7 +252,7 @@ class CounterFactualExplainerGradient:
             current_mf_values = self.compute_membership_values(fuzzy_sets, 0, 1)
             step = 1 / current_mf_values.shape[1]
 
-            intersection_value, union_value, mf_1_highest_area, mf_2_highest_area = (
+            intersection_value, union_value, mf_current_smallest_length, mf_current_highest_length = (
                 self.compute_membership_area_data(self._initial_mf_values, current_mf_values, step)
             )
 
@@ -304,8 +279,8 @@ class CounterFactualExplainerGradient:
                 mf_params,
                 intersection_value,
                 union_value,
-                mf_1_highest_area,
-                mf_2_highest_area,
+                mf_current_smallest_length,
+                mf_current_highest_length,
             )
             # print(f"Gradient: {gradient}")
 
@@ -368,6 +343,7 @@ class CounterFactualExplainerGradient:
 
 
 if __name__ == "__main__":
+
     # SIMPLE EXAMPLE
 
     # target_class = ClassLabelBasic(1)
@@ -426,7 +402,21 @@ if __name__ == "__main__":
     rule = sol1[0].get_var(0).get_rule()
 
     learner = LearningBasic(runner.get_train_set())
+
+    import time
+
+    start = time.time()
+
     explainer = CounterFactualExplainerGradient(
-        rule, ClassLabelBasic(1), runner.get_train_set(), learner, confidence_loss_weight=0.9, learning_rate=0.5, max_num_epochs=100
+        rule,
+        ClassLabelBasic(1),
+        runner.get_train_set(),
+        learner,
+        confidence_loss_weight=0.9,
+        learning_rate=0.5,
+        max_num_epochs=100,
     )
     explainer.get_counterfactual()
+
+    end = time.time()
+    print(f"Execution time: {end - start:.2f} seconds")

src/mofgbmlpy/explainer/gbml/operators/fuzzy_sets_mutation.py
@@ -29,8 +29,8 @@ class FuzzySetsMutation(Mutation):
                     elif isinstance(new_x[i][j].get_function(), DontCareMF):
                         # To triangular
                         left = np.random.rand()
-                        center = np.random.rand()*(1-left) + left
-                        right = np.random.rand()*(1-center) + center
+                        center = np.random.rand() * (1 - left) + left
+                        right = np.random.rand() * (1 - center) + center
 
                         new_x[i][j] = TriangularFuzzySet(left, center, right, 1, "new_term")
                 else:

src/mofgbmlpy/explainer/gbml/operators/fuzzy_sets_survival.py
@@ -1,5 +1,6 @@
 """
-The code in this file is mainly copied from the Pymoo library, since the function _do of the RankAndCrowding class is protected
+The code in this file is mainly copied from the Pymoo library,
+since the function _do of the RankAndCrowding class is protected
 """
 
 import numpy as np
@@ -18,12 +19,7 @@ class FuzzySetsSurvival(Survival):
         self.nds = nds if nds is not None else NonDominatedSorting()
         self.crowding_func = crowding_func_
 
-    def _do(self,
-            problem,
-            pop,
-            *args,
-            n_survive=None,
-            **kwargs):
+    def _do(self, problem, pop, *args, n_survive=None, **kwargs):
 
         # get the objective space values and objects
         F = pop.get("F").astype(float, copy=False)
@@ -36,32 +32,24 @@ class FuzzySetsSurvival(Survival):
 
         for k, front in enumerate(fronts):
 
-            I = np.arange(len(front))
+            indices_I = np.arange(len(front))
 
             # current front sorted by crowding distance if splitting
-            if len(survivors) + len(I) > n_survive:
+            if len(survivors) + len(indices_I) > n_survive:
 
                 # Define how many will be removed
                 n_remove = len(survivors) + len(front) - n_survive
 
                 # re-calculate the crowding distance of the front
-                crowding_of_front = \
-                    self.crowding_func.do(
-                        F[front, :],
-                        n_remove=n_remove
-                    )
+                crowding_of_front = self.crowding_func.do(F[front, :], n_remove=n_remove)
 
-                I = randomized_argsort(crowding_of_front, order='descending', method='numpy')
-                I = I[:-n_remove]
+                indices_I = randomized_argsort(crowding_of_front, order="descending", method="numpy")
+                indices_I = indices_I[:-n_remove]
 
             # otherwise take the whole front unsorted
             else:
                 # calculate the crowding distance of the front
-                crowding_of_front = \
-                    self.crowding_func.do(
-                        F[front, :],
-                        n_remove=0
-                    )
+                crowding_of_front = self.crowding_func.do(F[front, :], n_remove=0)
 
             # save rank and crowding in the individual class
             for j, i in enumerate(front):
@@ -69,7 +57,7 @@ class FuzzySetsSurvival(Survival):
                 pop[i].set("crowding", crowding_of_front[j])
 
             # extend the survivors by all or selected individuals
-            survivors.extend(front[I])
+            survivors.extend(front[indices_I])
 
         survivors = pop[survivors]
         survivors = self._eliminate_duplicates.do(survivors)

src/mofgbmlpy/explainer/gbml/problem/counterfactual_problem.py
@@ -29,9 +29,7 @@ class CounterfactualProblem(Problem):
 
         self._initial_mfs_y = self.compute_membership_values(self._initial_fuzzy_sets, 0, 1)
 
-        super().__init__(
-            n_var=n_vars, n_obj=2, xl=0, xu=1, n_eq_constr=1
-        )
+        super().__init__(n_var=n_vars, n_obj=2, xl=0, xu=1, n_eq_constr=1)
 
     def get_initial_mfs_y(self):
         return self._initial_mfs_y
@@ -74,7 +72,7 @@ class CounterfactualProblem(Problem):
                 union_value[fs_i] = 1
                 intersection_value[fs_i] = 1
 
-        return intersection_value/union_value
+        return intersection_value / union_value
 
     @staticmethod
     def build_antecedent(fuzzy_sets):
@@ -98,7 +96,8 @@ class CounterfactualProblem(Problem):
         antecedent = self.build_antecedent(fuzzy_sets)
 
         # Confidence loss
-        # We want to minimize the confidence difference between the initial class and the target class and we want to maximize the confidence of the target class
+        # We want to minimize the confidence difference between the initial class
+        # and the target class and we want to maximize the confidence of the target class
 
         # TODO: to be optimized, because for now all confidences are computed
         confidences = self._learner.calc_confidence_py(antecedent, self._train_set)
@@ -116,9 +115,7 @@ class CounterfactualProblem(Problem):
         current_mf_values = self.compute_membership_values(fuzzy_sets, 0, 1)
         step = 1 / current_mf_values.shape[1]
 
-        iou = (
-            self.compute_iou(self._initial_mfs_y, current_mf_values, step)
-        )
+        iou = self.compute_iou(self._initial_mfs_y, current_mf_values, step)
 
         if iou is not None:
             change_loss = 1 - np.mean(iou)
@@ -147,6 +144,5 @@ class CounterfactualProblem(Problem):
             out["F"][i][1] = change_loss
             out["H"][i] = 0 if output_class_is_target else 1
 
-
     def get_objective_names(self):
-        return ["conf_loss", "change_loss"]
+        return ["1 - target class confidence", "1 - IoU"]